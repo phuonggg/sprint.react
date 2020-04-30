000ce247df46f20ba0c62071036225a1f174c3d0
phuonggg <tieuphuong.ftu@gmail.com>
2020-04-30 22:31:00 +0900

    working on displaying photos

--- src/components/AllPhotos.jsx
+++ src/components/AllPhotos.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import _ from "lodash";
+import { getSingleObject } from "../utils/index.js";
 
-export default function AllPhotos() {
-  return <div />;
+export default function AllPhotos(photos) {
+  return (
+    <div>
+      photos.map((photo) => (
+      <img src={`data:image/jpeg;base64${photo}`} onClick={() => {}} />
+      ))}
+    </div>
+  );
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/styles.css";
+import Navbar from "./Navbar.jsx";
+import AllPhotos from "./AllPhotos.jsx";
+import SinglePhoto from "./SinglePhoto.jsx";
+import Upload from "./Upload.jsx";
+import { listObjects } from "../utils/index.js";
+import { getSingleObject } from "../utils/index.js";
+import ReactDOM from "react-dom";
 
 export default function App() {
+  const [currentView, setCurrentView] = useState("AllPhotos");
+  const [photos, setPhotos] = useState([]);
+  const [selectedPhotos, setSelectedPhotos] = useState("");
+
+  async function getPhotos() {
+    let list = await listObjects();
+    const array = list.map(obj => {
+      return getSingleObject(obj.Key);
+    });
+    Promise.all(array).then(item => {
+      setPhotos(item);
+    });
+  }
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+      <AllPhotos photos={photos} />
     </div>
   );
 }
